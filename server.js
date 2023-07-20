a75121f67e3bde6c8221b9e747b485c05e687cc9
VIKALP <113850666+vikalp026var@users.noreply.github.com>
2023-07-20 07:52:26 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
-const express= require('express')
+const express=require('express')
 const app=express()
-const PORT=process.env.PORT||3000;
-let counter=0;
-app.use(express.json());
+const PORT=process.env.PORT||8000;
 app.get('/',(req,res)=>{
-     res.json({counter});
+     res.send("I am homepage")
 });
-app.get('/increment',(req,res)=>{
-     counter++;
-     res.json({counter});
-})
-app.get('/decrement',(res,req)=>{
-     counter--;
-     res.json({counter})
+app.get('/about',(req,res)=>{
+     res.send('I am about Page ')
+});
+app.get('/contact',(req,res)=>{
+     res.send(' <a href="mailto:support@pwskills.com">suppor@pwskills.com</a>')
 })
 app.listen(PORT,()=>{
-     console.log(`Server is running on the port ${PORT}`);
+     console.log(`Server is running on the PORT ${PORT} `);
 })
